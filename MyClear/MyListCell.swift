47463b3a8871dfefdd4e72311374c5e2cf6188a7
BoLi <libo4101001@gmail.com>
2015-01-28 22:25:45 +0800

    添加下拉返回homepage的操作

---
+++ MyClear/MyListCell.swift
@@ -0,0 +1,9 @@
+//
+//  MyListCell.swift
+//  MyClear
+//
+//  Created by boli on 1/21/15.
+//  Copyright (c) 2015 boli. All rights reserved.
+//
+
+import Foundation
---
+++ TodoThing.m
@@ -0,0 +1,18 @@
+//
+//  TodoThing.m
+//  MyClear
+//
+//  Created by boli on 1/22/15.
+//  Copyright (c) 2015 boli. All rights reserved.
+//
+
+#import "TodoThing.h"
+
+
+@implementation TodoThing
+
+@dynamic id;
+@dynamic thing;
+@dynamic deadline;
+
+@end
